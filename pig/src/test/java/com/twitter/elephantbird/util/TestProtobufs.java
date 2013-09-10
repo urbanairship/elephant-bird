b0d5c096bba0919872dded0a892c7e5b838879c8
Dan Colish <dcolish@urbanairship.com>
2013-09-10 08:50:56 -0700

    Support deeply nested inner classes when searching classnames
    Protobufs support deep message nesting, greater than one deep, but the
    current inner class search implementation only looks one level deep and
    gives up. This new method adds support for arbitrarily deep nesting.
    Hopefully there are few cases where more than 2 levels are required
    since this method is fairly expensive.

--- pig/src/test/java/com/twitter/elephantbird/util/TestProtobufs.java
+++ pig/src/test/java/com/twitter/elephantbird/util/TestProtobufs.java
@@ -1,16 +1,17 @@
 package com.twitter.elephantbird.util;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
-import com.google.common.base.Function;
 import com.google.protobuf.Message;
 import com.twitter.data.proto.tutorial.AddressBookProtos.AddressBook;
 import com.twitter.data.proto.tutorial.AddressBookProtos.Person;
 import com.twitter.elephantbird.mapreduce.io.ProtobufConverter;
 import com.twitter.elephantbird.pig.piggybank.Fixtures;
-import com.twitter.elephantbird.util.Protobufs;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.internal.matchers.StringContains;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TestProtobufs {
 
@@ -20,8 +21,8 @@ public class TestProtobufs {
   @Test
   public void testGetInnerProtobufClass() {
     String canonicalClassName = "com.twitter.data.proto.tutorial.AddressBookProtos.Person";
-    Class<? extends Message> klass = Protobufs.getInnerProtobufClass(canonicalClassName);
-    assertEquals(klass, Person.class);
+    Class<? extends Message> klass = Protobufs.getByCanonicalClassName(canonicalClassName);
+    assertEquals(Person.class, klass);
   }
 
   @Test
@@ -39,4 +40,31 @@ public class TestProtobufs {
     ProtobufConverter<AddressBook> protoConverter = ProtobufConverter.newInstance(AddressBook.class);
     assertEquals(ab_, protoConverter.fromBytes(abBytes_));
   }
+
+  @Test
+  public void testGetInnerInnerProtobufClass() {
+    String canonicalClassName = "com.twitter.data.proto.tutorial.AddressBookProtos.Person.PhoneNumber";
+    Class<? extends Message> klass = Protobufs.getByCanonicalClassName(canonicalClassName);
+    assertEquals(Person.PhoneNumber.class, klass);
+  }
+
+  @Test
+  public void testNotFoundInnerInnerProtobufClass() {
+    String canonicalClassName = "com.twitter.data.proto.tutorial.AddressBookProtos.Person.Nope";
+    Class<? extends Message> klass = Protobufs.getByCanonicalClassName(canonicalClassName);
+    assertNull(klass);
+  }
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void testNotFoundOuterInnerProtobufClass() {
+    thrown.expect(IllegalArgumentException.class);
+    thrown.expectMessage(StringContains.containsString("Could not find"));
+    String canonicalClassName = "com.twitter.data.proto.tutorial.LolNope";
+    Class<? extends Message> klass = Protobufs.getByCanonicalClassName(canonicalClassName);
+    assertNull(klass);
+  }
+
 }
